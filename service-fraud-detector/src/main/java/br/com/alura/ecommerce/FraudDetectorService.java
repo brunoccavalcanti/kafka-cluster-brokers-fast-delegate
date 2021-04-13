5ee47353693c75e89bce1eca7eaa0c91dcdc9645
Bruno Cavalcanti <brunoc.cavalcanti22@gmail.com>
2021-04-13 10:44:22 -0300

    Pattern Eager and consumers producing

--- service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ service-fraud-detector/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -2,7 +2,9 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.math.BigDecimal;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class FraudDetectorService {
 
@@ -17,7 +19,9 @@ public class FraudDetectorService {
         }
     }
 
-    private void parse(ConsumerRecord<String, Order> record) {
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<Order>();
+
+    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException {
         System.out.println("------------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
@@ -30,7 +34,22 @@ public class FraudDetectorService {
             // ignoring
             e.printStackTrace();
         }
+        var order = record.value();
+        if (isFraud(order)) {
+            // Pretending that the fraud happens when the amount is >= 4500
+            System.out.println("Order is a fraud!!!");
+            orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getUserId(), order);
+        }else{
+            System.out.println("Approved " + order);
+            orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getUserId(), order);
+
+        }
+
         System.out.println("Order processed");
+    }
+
+    private boolean isFraud(Order order) {
+        return order.getAmount().compareTo(new BigDecimal("4500")) >= 0;
     }
 
 }
