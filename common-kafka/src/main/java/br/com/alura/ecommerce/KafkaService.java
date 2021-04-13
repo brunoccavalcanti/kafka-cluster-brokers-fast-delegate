f881be232576a2ead08c5ddd28dcfa004964b5a6
Bruno Cavalcanti <brunoc.cavalcanti22@gmail.com>
2021-04-13 14:52:30 -0300

    Added Create User sevice using sqlite

--- common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -40,10 +40,8 @@ class KafkaService<T> implements Closeable {
                 for (var record : records) {
                     try {
                         parse.consume(record);
-                    } catch (ExecutionException e) {
-                        // so far, just logging the exception for this message
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
+                    } catch (Exception e) {
+                        // only catches Exception because no matter wich Exception I want to recover and parse the next one
                         // so far, just logging the exception for this message
                         e.printStackTrace();
                     }
